96686d4f445b38de9c5f97d481b472b4319892c5
berlin <bolin.hong@outlook.com>
2024-10-18 22:01:59 +0800

    取消使用Spring Security

--- abner-test/src/main/java/com/hongyou/abner/operator/OperatorControl.java
+++ abner-test/src/main/java/com/hongyou/abner/operator/OperatorControl.java
@@ -3,31 +3,22 @@
  */
 package com.hongyou.abner.operator;
 
-import com.hongyou.abner.data.DataProvider;
-import com.hongyou.abner.data.model.Csttrc;
-import com.hongyou.abner.data.model.Unitms;
-import com.mybatisflex.core.query.QueryWrapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author Berlin
  */
 @RestController
 @RequestMapping("/")
-public class OperatorControl extends DataProvider {
+public class OperatorControl {
 
     private static final Logger logger = LoggerFactory.getLogger(OperatorControl.class);
 
     @RequestMapping("/dbtest")
-    public List<Unitms> getByAccount() {
-        List<Unitms> list = this.db().unitms().list(new QueryWrapper());
-        logger.info(list.get(0).getCretby() + Csttrc.STATUS.PROCESSED);
-        return new ArrayList<>();
+    public String getByAccount() {
+        return "hello";
     }
 }
