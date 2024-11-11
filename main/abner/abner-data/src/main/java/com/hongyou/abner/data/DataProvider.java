27f3f080dfb311dfc0b734bc1bdd2b0184ef5375
berlin <bolin.hong@outlook.com>
2024-11-11 22:18:51 +0800

    文件目录结构变更

---
+++ main/abner/abner-data/src/main/java/com/hongyou/abner/data/DataProvider.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2024, Hongyou Software Development Studio.
+ */
+package com.hongyou.abner.data;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @author Berlin
+ */
+public class DataProvider {
+
+    /**
+     * 数据库组件
+     */
+    private MOMDB momdb;
+
+    /**
+     * 注入数据库组件
+     */
+    @Autowired
+    public void setMOMDB(MOMDB momdb) {
+        this.momdb = momdb;
+    }
+
+    /**
+     * 获取数据库组件
+     */
+    public MOMDB db() {
+        return momdb;
+    }
+}
---
+++ main/abner/abner-main/src/main/java/com/hongyou/abner/AbnerMainApplication.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2024, Hongyou Software Development Studio.
+ */
+package com.hongyou.abner;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * 后台应用程序启动入口
+ *
+ * @author berlin
+ */
+@SpringBootApplication
+@MapperScan("com.hongyou.abner.data.mapper")
+@ComponentScan(basePackages = {"com.hongyou.baron", "com.hongyou.abner"})
+public class AbnerMainApplication {
+
+    /**
+     * 应用程序启动
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(AbnerMainApplication.class, args);
+    }
+}
