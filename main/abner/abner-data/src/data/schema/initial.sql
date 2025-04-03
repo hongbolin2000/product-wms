f552dfc5b5abb15ffe2b11279573d8ec6cf2146b
berlin <bolin.hong@outlook.com>
2025-04-03 17:57:35 +0800

    公司管理

--- main/abner/abner-data/src/data/schema/initial.sql
+++ main/abner/abner-data/src/data/schema/initial.sql
@@ -13,3 +13,6 @@ insert into pmsnms(cmpnid, sortng, pmsncd, remark, actcde, actnam) values (1, 4,
 insert into pmsnms(cmpnid, sortng, pmsncd, remark, actcde, actnam) values (1, 5, 'sy02', '用户管理', 'frozen', '冻结');
 insert into pmsnms(cmpnid, sortng, pmsncd, remark, actcde, actnam) values (1, 6, 'sy02', '用户管理', 'unfrozen', '解除冻结');
 insert into pmsnms(cmpnid, sortng, pmsncd, remark, actcde, actnam) values (1, 7, 'sy02', '用户管理', 'reset.pwd', '重置密码');
+
+insert into pmsnms(cmpnid, sortng, pmsncd, remark, actcde, actnam) values (1, 1, 'sy03', '公司管理', 'browse', '查看');
+insert into pmsnms(cmpnid, sortng, pmsncd, remark, actcde, actnam) values (1, 2, 'sy03', '公司管理', 'update', '修改');
---
+++ main/abner/abner-sy/src/main/java/com/hongyou/abner/sy03/SY03.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2014, Chengyou Software Development Studio.
+ */
+package com.hongyou.abner.sy03;
+
+import com.hongyou.abner.config.event.EventLog;
+import com.hongyou.abner.config.web.UserDataProvider;
+import com.hongyou.abner.data.model.Cmpnms;
+import com.hongyou.abner.data.model.Userms;
+import com.hongyou.abner.data.pojo.CmpnmsPojo;
+import com.hongyou.baron.exceptions.RestRuntimeException;
+import com.hongyou.baron.logging.Log;
+import com.hongyou.baron.logging.LogFactory;
+import com.hongyou.baron.util.ObjectUtil;
+import com.hongyou.baron.util.StringUtil;
+import com.hongyou.baron.web.ResponseEntry;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Timestamp;
+import java.util.Map;
+
+/**
+ * 公司管理
+ *
+ * @author Hong Bo Lin
+ */
+@RestController
+@RequestMapping("/sy03")
+public class SY03 extends UserDataProvider {
+
+    /**
+     * logger
+     */
+    private static final Log logger = LogFactory.getLog(SY03.class);
+
+    /**
+     * 保存公司
+     */
+    @PostMapping("/save")
+    @Transactional(rollbackFor = RestRuntimeException.class)
+    public ResponseEntry save(
+            @RequestBody final CmpnmsPojo cmpnmsPojo, final HttpServletRequest request
+    ) {
+
+        try {
+            Userms loginUser = this.getLoginUser();
+            String operatorBy = this.getOperatorBy(loginUser);
+            Timestamp currentTime = this.getCurrentTime();
+
+            Cmpnms cmpnms = this.db().cmpnms().get(cmpnmsPojo.getId());
+            Cmpnms oldCmpnms = (Cmpnms) cmpnms.clone();
+
+            // 检查是否已存在
+            if (!ObjectUtil.equal(cmpnmsPojo.getCompanyCode(), cmpnms.getCmpncd())) {
+                Cmpnms existed = this.db().cmpnms().getByCode(cmpnmsPojo.getCompanyCode());
+                if (ObjectUtil.isNotNull(existed)) {
+                    return ResponseEntry.builder().code(-1).message("公司编号已存在").build();
+                }
+            }
+
+            cmpnms.cmpncd(cmpnmsPojo.getCompanyCode()).
+                    cmpnnm(cmpnmsPojo.getCompanyName()).
+                    cmpnty(cmpnmsPojo.getCompanyType()).
+                    cmpmpf(cmpnmsPojo.getCompanyProfile()).
+                    indsty(cmpnmsPojo.getIndustry()).
+                    ofcwbs(cmpnmsPojo.getOfficialWebsite()).
+                    addres(cmpnmsPojo.getAddress()).
+                    contcs(cmpnmsPojo.getContacts()).
+                    phonno(cmpnmsPojo.getPhoneNo()).
+                    email(cmpnmsPojo.getZipCode()).
+                    zipcde(cmpnmsPojo.getZipCode()).
+                    pftitl(cmpnmsPojo.getPlatformTitle()).
+                    pfstil(cmpnmsPojo.getPlatformSubtitle()).
+                    pfsptl(cmpnmsPojo.getPlatformSimpleTitle()).
+                    capvrf(cmpnmsPojo.getCaptchaVerify()).
+                    autlgn(cmpnmsPojo.getAutoLogin()).
+                    rmbusn(cmpnmsPojo.getRememberUsername()).
+                    rmbpsw(cmpnmsPojo.getRememberPassword()).
+                    remark(cmpnmsPojo.getRemark()).
+                    oprtby(operatorBy).
+                    oprttm(currentTime);
+            this.db().cmpnms().save(cmpnms);
+
+            // 记录日志
+            Map<String, String> displays = this.international.getTableValuesDisplay(request, "cmpnms");
+            EventLog event = EventLog.builder().
+                    domain(loginUser.getCmpnid()).
+                    operator(operatorBy).
+                    module(SY03.class.getSimpleName()).
+                    name("公司管理").
+                    action("修改").
+                    message(StringUtil.format("公司[{}]修改成功", cmpnms.getCmpnnm())).
+                    oldValue(oldCmpnms).
+                    newValue(cmpnms).
+                    enumsDisplay(displays).
+                    build();
+            this.eventLogManager.info(event);
+
+            return ResponseEntry.SUCCESS;
+        } catch (Exception e) {
+            logger.error("公司保存失败", e);
+            throw new RestRuntimeException("公司保存失败");
+        }
+    }
+}
