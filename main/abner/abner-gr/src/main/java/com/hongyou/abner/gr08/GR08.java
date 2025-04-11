c60af181a93c641c68be6a637ad217007023ef7a
berlin <bolin.hong@outlook.com>
2025-04-11 16:43:43 +0800

    发货

--- main/abner/abner-gr/src/main/java/com/hongyou/abner/gr08/GR08.java
+++ main/abner/abner-gr/src/main/java/com/hongyou/abner/gr08/GR08.java
@@ -14,8 +14,6 @@ import com.hongyou.baron.util.ObjectUtil;
 import com.hongyou.baron.util.StringUtil;
 import com.hongyou.baron.web.ResponseEntry;
 import com.mybatisflex.core.query.QueryWrapper;
-import com.mybatisflex.core.row.Db;
-import com.mybatisflex.core.row.Row;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -50,133 +48,129 @@ public class GR08 extends UserDataProvider {
     public ResponseEntry batchReceive(@RequestBody final ReceiveLinePojo pojo) {
 
         try {
-            List<Row> rows = Db.selectListBySql("select bincde, sum(onhdqt) as onhdqt from vstckim where onhdqt > 0 and skuno = 'GX00000001' group by bincde");
-            int a = 0;
+            Userms loginUser = this.getLoginUser();
+            String operatorBy = this.getOperatorBy(loginUser);
+            Timestamp currentTime = this.getCurrentTime();
 
+            Roline roline = this.db().roline().get(pojo.getId());
+            Rohead rohead = this.db().rohead().get(roline.getRohdid());
+            Binmas binmas = this.db().binmas().get(pojo.getBinId());
 
-//            Userms loginUser = this.getLoginUser();
-//            String operatorBy = this.getOperatorBy(loginUser);
-//            Timestamp currentTime = this.getCurrentTime();
-//
-//            Roline roline = this.db().roline().get(pojo.getId());
-//            Rohead rohead = this.db().rohead().get(roline.getRohdid());
-//            Binmas binmas = this.db().binmas().get(pojo.getBinId());
-//
-//            // 生成批次号
-//            if (StringUtil.isBlank(roline.getBathno())) {
-//                String batchNo = DateUtil.format(currentTime, "yyMMddHHmmss");
-//                roline.bathno(batchNo).
-//                        status(Roline.STATUS.Receiving);
-//            }
-//
-//            roline.rcvqty(roline.getRcvqty().add(pojo.getReceiveQty())).
-//                    status(Roline.STATUS.Receiving);
-//
-//            // 收货单行完成收货
-//            if (roline.getRcvqty().compareTo(roline.getOrdqty()) >= 0) {
-//                roline.status(Roline.STATUS.Finished);
-//            }
-//            roline.oprtby(operatorBy).
-//                    oprttm(currentTime);
-//            this.db().roline().save(roline);
-//
-//            // 收货单开始收货
-//            if (StringUtil.isBlank(rohead.getRecvby())) {
-//                rohead.status(Rohead.STATUS.Receiving).
-//                        recvby(operatorBy).
-//                        recvdt(new Date(currentTime.getTime()));
-//            }
-//
-//            // 收货单完成收货
-//            QueryWrapper wrapper = QueryWrapper.create();
-//            wrapper.and(ROLINE.ROHDID.eq(rohead.getRohdid())).
-//                    and(ROLINE.STATUS.eq(Roline.STATUS.No).or(ROLINE.STATUS.eq(Roline.STATUS.Receiving)));
-//
-//            List<Roline> rolines = this.db().roline().list(wrapper);
-//            if (ListUtil.isEmpty(rolines)) {
-//                rohead.status(Rohead.STATUS.Finished);
-//            }
-//            rohead.oprtby(operatorBy).
-//                    oprttm(currentTime);
-//            this.db().rohead().save(rohead);
-//
-//            // 生成库存唯一码
-//            String stockItemNo = pojo.getSerialNo();
-//            if (StringUtil.isBlank(stockItemNo)) {
-//                stockItemNo = DateUtil.format(currentTime, "yyMMddHHmmss");
-//            }
-//
-//            // 保存收货单项
-//            Roitem roitem = new Roitem();
-//            roitem.rolnid(roline.getRolnid()).
-//                    stimno(stockItemNo).
-//                    binmid(binmas.getBinmid()).
-//                    cartno(pojo.getCartonNo()).
-//                    rcvqty(pojo.getReceiveQty()).
-//                    rcvtim(currentTime).
-//                    recvby(operatorBy).
-//                    remark(pojo.getRemark()).
-//                    oprtby(operatorBy).
-//                    oprttm(currentTime);
-//            this.db().roitem().save(roitem);
-//
-//            // 生成库存主信息
-//            Stckms stckms = this.db().stckms().getByMaterial(rohead.getWrhsid(), roline.getMtrlid());
-//            if (ObjectUtil.isNull(stckms)) {
-//                stckms = new Stckms();
-//                stckms.wrhsid(rohead.getWrhsid()).
-//                        mtrlid(roline.getMtrlid()).
-//                        cretby(operatorBy).
-//                        crettm(currentTime);
-//            }
-//
-//            // 更新库存主信息
-//            stckms.onhdqt(stckms.getOnhdqt().add(pojo.getReceiveQty())).
-//                    oprtby(operatorBy).
-//                    oprttm(currentTime);
-//            this.db().stckms().save(stckms);
-//
-//            // 生成库存项
-//            Stckim stckim = new Stckim();
-//            stckim.stckid(stckms.getStckid()).
-//                    price(roline.getPrice()).
-//                    wrhsid(rohead.getWrhsid()).
-//                    stimno(stockItemNo).
-//                    rdocty(rohead.getRdocty()).
-//                    rcvqty(pojo.getReceiveQty()).
-//                    rvodno(rohead.getRvodno()).
-//                    rcvtim(currentTime).
-//                    rcvdat(new Date(currentTime.getTime())).
-//                    recvby(operatorBy).
-//                    suplid(rohead.getSuplid()).
-//                    projid(rohead.getProjid()).
-//                    cstmid(rohead.getCstmid()).
-//                    bathno(roline.getBathno()).
-//                    binmid(pojo.getBinId()).
-//                    cartno(pojo.getCartonNo()).
-//                    onhdqt(pojo.getReceiveQty()).
-//                    remark(pojo.getRemark()).
-//                    oprtby(operatorBy).
-//                    oprttm(currentTime);
-//            this.db().stckim().save(stckim);
-//
-//            // 生成库存日志
-//            Stcktn stcktn = new Stcktn();
-//            stcktn.wrhsid(rohead.getWrhsid()).
-//                    trntyp(Stcktn.TRNTYP.Receive).
-//                    rdocty(rohead.getRdocty()).
-//                    rdocno(rohead.getRdocno()).
-//                    stimno(stckim.getStimno()).
-//                    mtrlid(roline.getMtrlid()).
-//                    price(roline.getPrice()).
-//                    rvodno(rohead.getRvodno()).
-//                    bathno(roline.getBathno()).
-//                    bincde(binmas.getBincde()).
-//                    cartno(pojo.getCartonNo()).
-//                    nohdqt(pojo.getReceiveQty()).
-//                    oprtby(operatorBy).
-//                    oprttm(currentTime);
-//            this.db().stcktn().save(stcktn);
+            // 生成批次号
+            if (StringUtil.isBlank(roline.getBathno())) {
+                String batchNo = DateUtil.format(currentTime, "yyMMddHHmmss");
+                roline.bathno(batchNo).
+                        status(Roline.STATUS.Receiving);
+            }
+
+            roline.rcvqty(roline.getRcvqty().add(pojo.getReceiveQty())).
+                    status(Roline.STATUS.Receiving);
+
+            // 收货单行完成收货
+            if (roline.getRcvqty().compareTo(roline.getOrdqty()) >= 0) {
+                roline.status(Roline.STATUS.Finished);
+            }
+            roline.oprtby(operatorBy).
+                    oprttm(currentTime);
+            this.db().roline().save(roline);
+
+            // 收货单开始收货
+            if (StringUtil.isBlank(rohead.getRecvby())) {
+                rohead.status(Rohead.STATUS.Receiving).
+                        recvby(operatorBy).
+                        recvdt(new Date(currentTime.getTime()));
+            }
+
+            // 收货单完成收货
+            QueryWrapper wrapper = QueryWrapper.create();
+            wrapper.and(ROLINE.ROHDID.eq(rohead.getRohdid())).
+                    and(ROLINE.STATUS.eq(Roline.STATUS.No).or(ROLINE.STATUS.eq(Roline.STATUS.Receiving)));
+
+            List<Roline> rolines = this.db().roline().list(wrapper);
+            if (ListUtil.isEmpty(rolines)) {
+                rohead.status(Rohead.STATUS.Finished);
+            }
+            rohead.oprtby(operatorBy).
+                    oprttm(currentTime);
+            this.db().rohead().save(rohead);
+
+            // 生成库存唯一码
+            String stockItemNo = pojo.getSerialNo();
+            if (StringUtil.isBlank(stockItemNo)) {
+                stockItemNo = DateUtil.format(currentTime, "yyMMddHHmmss");
+            }
+
+            // 保存收货单项
+            Roitem roitem = new Roitem();
+            roitem.rolnid(roline.getRolnid()).
+                    stimno(stockItemNo).
+                    binmid(binmas.getBinmid()).
+                    cartno(pojo.getCartonNo()).
+                    rcvqty(pojo.getReceiveQty()).
+                    rcvtim(currentTime).
+                    recvby(operatorBy).
+                    remark(pojo.getRemark()).
+                    oprtby(operatorBy).
+                    oprttm(currentTime);
+            this.db().roitem().save(roitem);
+
+            // 生成库存主信息
+            Stckms stckms = this.db().stckms().getByMaterial(rohead.getWrhsid(), roline.getMtrlid());
+            if (ObjectUtil.isNull(stckms)) {
+                stckms = new Stckms();
+                stckms.wrhsid(rohead.getWrhsid()).
+                        mtrlid(roline.getMtrlid()).
+                        cretby(operatorBy).
+                        crettm(currentTime);
+            }
+
+            // 更新库存主信息
+            stckms.onhdqt(stckms.getOnhdqt().add(pojo.getReceiveQty())).
+                    oprtby(operatorBy).
+                    oprttm(currentTime);
+            this.db().stckms().save(stckms);
+
+            // 生成库存项
+            Stckim stckim = new Stckim();
+            stckim.stckid(stckms.getStckid()).
+                    price(roline.getPrice()).
+                    wrhsid(rohead.getWrhsid()).
+                    stimno(stockItemNo).
+                    rdocty(rohead.getRdocty()).
+                    rcvqty(pojo.getReceiveQty()).
+                    rvodno(rohead.getRvodno()).
+                    rcvtim(currentTime).
+                    rcvdat(new Date(currentTime.getTime())).
+                    recvby(operatorBy).
+                    suplid(rohead.getSuplid()).
+                    projid(rohead.getProjid()).
+                    cstmid(rohead.getCstmid()).
+                    bathno(roline.getBathno()).
+                    binmid(pojo.getBinId()).
+                    cartno(pojo.getCartonNo()).
+                    onhdqt(pojo.getReceiveQty()).
+                    remark(pojo.getRemark()).
+                    oprtby(operatorBy).
+                    oprttm(currentTime);
+            this.db().stckim().save(stckim);
+
+            // 生成库存日志
+            Stcktn stcktn = new Stcktn();
+            stcktn.wrhsid(rohead.getWrhsid()).
+                    trntyp(Stcktn.TRNTYP.Receive).
+                    rdocty(rohead.getRdocty()).
+                    rdocno(rohead.getRdocno()).
+                    stimno(stckim.getStimno()).
+                    mtrlid(roline.getMtrlid()).
+                    price(roline.getPrice()).
+                    rvodno(rohead.getRvodno()).
+                    bathno(roline.getBathno()).
+                    bincde(binmas.getBincde()).
+                    cartno(pojo.getCartonNo()).
+                    nohdqt(pojo.getReceiveQty()).
+                    oprtby(operatorBy).
+                    oprttm(currentTime);
+            this.db().stcktn().save(stcktn);
 
             return ResponseEntry.SUCCESS;
         } catch (Exception e) {
