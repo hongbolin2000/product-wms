493ba6635624f61536eb78c401c7995d85528536
berlin <bolin.hong@outlook.com>
2025-05-08 15:36:27 +0800

    删除通用界面示例

--- web/abner-admin/src/routes/index.ts
+++ web/abner-admin/src/routes/index.ts
@@ -10,7 +10,6 @@ import {ThemeLayout} from '@/ploutos';
  *******************************************************************************/
 import SY01 from '@/views/sy/sy01/routes.ts';
 import SY02 from '@/views/sy/sy02/routes.ts';
-import SY99 from '@/views/sy/sy99/routes.ts';
 
 /**
  * 路由配置
@@ -21,8 +20,7 @@ const index: RouteRecordRaw[] = [
     component: () => ThemeLayout,
     children: [
       ...SY01,
-      ...SY02,
-      ...SY99
+      ...SY02
     ]
   }
 ]
